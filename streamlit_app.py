b608f13087cd05622db59d6987e317b997e9a1d6
Brandon-Mekcer <brandon.musyoka@strathmore.edu>
2025-06-12 21:46:44 +0300

    Comments added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
+import datetime
 
-st.title("🎈 My new app")
+min_date = datetime.date(1900, 1, 1)
+max_date = datetime.date(2100, 12, 12)
+today = datetime.datetime.now
+
+st.title("🎈Title: Bio Data")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    "This is a sample web app")
+
+first_name = st.text_input("First Name")
+last_name = st.text_input("Last Name")
+gender = st.selectbox("Gender", ["Male", "Female"])
+age = st.number_input("Your age", 0, 100, 30, 1)
+dob = st.date_input("Your birthday", "today", min_date, max_date)
+marital_status = st.radio("Marital status", ["Single", "Married"])
+years_of_experience = st.slider("Years of experience", 0, 40)
--- web_app.py
+++ web_app.py
@@ -1,17 +1,22 @@
 import pickle
 import numpy as np
 import streamlit as st
+
+# import the trained linear regression from model.py
 from model import regModel
 
-# model = pickle.load(open('model.pkl', 'rb'))
 
+# we format the layout so as to center the content
 col0, col1, col2, col3, col4, col5, col6 = st.columns([1,1,1,2,1,1,1])
+
+#we empty columns for spacing and aligning
 with col0:
     st.write('')
 with col1:
     st.write('')
 with col2:
-    st.write('')    
+    st.write('') 
+#the title at the center   
 with col3:
     st.title("ⴍage") 
 with col4:
@@ -20,7 +25,7 @@ with col5:
     st.write('')
 with col6:
     st.write('')
-
+#centered title row.
 col7, col8, col9 = st.columns(3)
 with col7:
     st.write('')    
@@ -29,17 +34,22 @@ with col8:
 with col9:
     st.write('')
 
+#Options for the user to input
 gen_list = ["Female", "Male"]
 edu_list = ["Bachelor's", "Master's", "PhD"]
 job_list = ["Director of Marketing", "Director of Operations", "Senior Data Scientist", "Senior Financial Analyst", "Senior Software Engineer"]
+
+#matching each job title to a numeric code for prediction.
 job_idx = [0, 1, 10, 11, 20]
 
+#we put widgets in which users can input where they lie
 gender = st.radio('Pick your gender', gen_list)
 age = st.slider('Pick your age', 21, 55)
 education = st.selectbox('Pick your education level', edu_list)
 job = st.selectbox('Pick your job title', job_list)
 experience = st.slider('Pick your years of experience', 0.0, 25.0, 0.0, 0.5, "%1f")
 
+#we create empty columns to centre the predict button
 col10, col11, col12, col13, col14 = st.columns(5)
 with col10:
     st.write('')
@@ -55,14 +65,21 @@ with col14:
 if(predict_btn):
     inp1 = int(age)
     inp2 = float(experience)
+    #map a job to its numeric code
     inp3 = int(job_idx[job_list.index(job)])
+    #convert education to numeric index
     inp4 = int(edu_list.index(education))
+    #convert gender to numeric index
     inp5 = int(gen_list.index(gender))
+
+    #Put in the final input vector
     X = [inp1, inp2, inp3, inp4, inp5]
+    #predict the salary using the trained regression model
     salary = regModel.predict([X])
     col15, col16, col17 = st.columns(3)
     with col15:
-        st.write('')    
+        st.write('') 
+    #Display the result   
     with col16:
         st.text(f"Estimated salary: ${int(salary[0])}")
     with col17:
